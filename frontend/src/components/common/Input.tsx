042eb7ea7a83825fda8870635d8d810a385aa2b6
harshillvashisht <harshillvashisht58@gmail.com>
2026-07-18 11:18:33 +0530

    Polish input component

--- frontend/src/components/common/Input.tsx
+++ frontend/src/components/common/Input.tsx
@@ -11,14 +11,27 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         className={`
           w-full
-          rounded-lg
+          min-h-11
+          rounded-xl
           border
-          border-gray-300
+          border-slate-300
+          bg-white
           px-4
-          py-2
+          py-2.5
+          text-sm
+          text-slate-900
+          placeholder:text-slate-400
+          shadow-sm
           outline-none
           transition
+          duration-200
           focus:border-blue-500
+          focus:ring-4
+          focus:ring-blue-500/10
+          disabled:cursor-not-allowed
+          disabled:bg-slate-100
+          disabled:text-slate-500
+          disabled:opacity-70
           ${className}
         `}
         {...props}
