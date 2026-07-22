4083961c4c65f013d032529ab04346e4ee8c6f1f
harshillvashisht <harshillvashisht58@gmail.com>
2026-07-22 15:22:58 +0530

    Add production deployment and HTTPS configuration

--- frontend/src/congif.ts
+++ frontend/src/congif.ts
@@ -1,3 +1,3 @@
 
-export const SHORT_BASE_URL = import.meta.env.SHORT_BASE_URL || "http://localhost:3000";
+export const SHORT_BASE_URL = "https://go.harshilldev.me";
 export const API_BASE_URL = import.meta.env.API_BASE_URL || "/api/v1";
